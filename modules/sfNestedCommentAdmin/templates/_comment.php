f0846741d41c71836a05565ebd80bb18f3a9ee7e
nibsirahsieu <nibsirahsieu@gmail.com>
2011-01-15 01:02:48 +0700

    unescape comment content when it is rendering in the template

--- modules/sfNestedCommentAdmin/templates/_comment.php
+++ modules/sfNestedCommentAdmin/templates/_comment.php
@@ -4,4 +4,4 @@
 <div class="submitted-on">
   <?php echo format_datetime(strtotime($comment->getCreatedAt()), 'f') ?>
 </div>
-<?php echo $comment->getContent() ?>
+<?php echo $comment->getContent(ESC_RAW) ?>
