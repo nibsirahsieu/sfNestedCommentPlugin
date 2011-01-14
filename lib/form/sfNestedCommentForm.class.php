973c4527c29c08b22611e7a973b29704b1311ecd
nibsirahsieu <nibsirahsieu@gmail.com>
2011-01-15 00:48:52 +0700

    refactoring in the update content of an object

--- lib/form/sfNestedCommentForm.class.php
+++ lib/form/sfNestedCommentForm.class.php
@@ -28,24 +28,6 @@ class sfNestedCommentForm extends BasesfNestedCommentForm
 
   public function updateContentColumn($value)
   {
-    $allowed_html_tags = sfConfig::get('app_sfNestedComment_allowed_tags', array());
-    spl_autoload_register(array('HTMLPurifier_Bootstrap', 'autoload'));
-    $config = HTMLPurifier_Config::createDefault();
-    $config->set('HTML', 'Doctype', 'XHTML 1.0 Strict');
-    $config->set('HTML', 'Allowed', implode(',', array_keys($allowed_html_tags)));
-
-    if (isset($allowed_html_tags['a']))
-    {
-      $config->set('HTML', 'AllowedAttributes', 'a.href');
-      $config->set('AutoFormat', 'Linkify', true);
-    }
-
-    if (isset($allowed_html_tags['p']))
-    {
-      $config->set('AutoFormat', 'AutoParagraph', true);
-    }
-
-    $purifier = new HTMLPurifier($config);
-    return str_replace('<a href', '<a rel="nofollow" href', $purifier->purify($value));
+    return sfNestedCommentTools::clean($value);
   }
 }
