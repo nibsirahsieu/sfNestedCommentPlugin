e10005fd211de4843261d70c4bee96b93a145442
nibsirahsieu <nibsirahsieu@gmail.com>
2011-03-30 01:21:11 +0700

    [mail template] remove dependency to omCrossAppUrlPlugin

--- modules/sfNestedCommentMail/templates/_commentHtml.php
+++ modules/sfNestedCommentMail/templates/_commentHtml.php
@@ -11,12 +11,18 @@
 <br/>
 <?php echo __('Comment: ') ?>
 <br/>
-<?php echo $comment->getContent(ESC_RAW) ?>
+<?php echo $comment->getContent() ?>
 <br/><br/>
 
 <?php if($comment->getIsModerated()): ?>
   <?php echo __('This comment was automatically moderated and is waiting for your approval.') ?>
-<?php endif; ?>
-<?php if ($callable = sfConfig::get('app_sfNestedComment_url_method')): ?>
-  <a href="<?php echo url_for(call_user_func($callable, $comment->getCommentableObject()), true).'#comment-'.$comment->getId() ?>"><?php echo __('Go to comment') ?></a>
+  <br/>
+  <!-- note: if you are using different app for backend application, you can use omCrossAppUrlPlugin to enable cross linking application-->
+  <!-- example : $url_approve = cross_app_url_for('backend', '@sf_nested_comment_toggle_publish?id='.$comment->getId(), true) -->
+  <?php $url_approve = url_for('@sf_nested_comment_toggle_publish?id='.$comment->getId(), true) ?>
+  Approve it: <a href="<?php echo $url_approve ?>"><?php echo $url_approve ?></a>
+<?php else: ?>
+  <?php if ($callable = sfConfig::get('app_sfNestedComment_url_commentable_method')): ?>
+    <a href="<?php echo url_for(call_user_func($callable, $comment->getCommentableObject()), true).'#comment-'.$comment->getId() ?>"><?php echo __('Go to comment') ?></a>
+  <?php endif; ?>
 <?php endif; ?>
--- modules/sfNestedCommentMail/templates/_replyHtml.php
+++ modules/sfNestedCommentMail/templates/_replyHtml.php
@@ -1,7 +1,10 @@
 <?php use_helper('I18N') ?>
 <?php $commentableObject = $reply->getCommentableObject() ?>
-<?php $callable = sfConfig::get('app_sfNestedComment_url_method') ?>
-<?php $url_callable = $callable ? url_for(call_user_func($callable, $commentableObject), true) : '' ?>
+<?php $callable = sfConfig::get('app_sfNestedComment_url_commentable_method', false) ?>
+
+<!-- note: if you are using different app for frontend application, you can use omCrossAppUrlPlugin to enable cross linking application-->
+<!-- example : $url_approve = cross_app_url_for('frontend', call_user_func($callable, $commentableObject), true) -->
+<?php $url_callable = $callable ?  url_for(call_user_func($callable, $commentableObject), true) : '' ?>
 <?php if ($url_callable): ?>
   <strong><?php echo $reply->getAuthorName() ?></strong><?php echo __(' replied to your comment on ')?> <a href="<?php echo $url_callable ?>" ><?php echo $commentableObject->__toString() ?></a>
 <?php else: ?>
